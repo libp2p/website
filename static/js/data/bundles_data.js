4238b4cdce39a9061784803e96e17fb6d640b87b
jesseclay <jesseclay@gmail.com>
2017-04-03 12:54:35 -0400

    add bundles data json file

---
+++ static/js/data/bundles_data.js
@@ -0,0 +1,223 @@
+var data = {
+  'Bundles': {
+    'Browser JS': {
+      'status': 'live',
+      'image': '../img/logo_1.png',
+      'github': 'https://github.com/libp2p/js-libp2p',
+      'categories': {
+        'Transport': {
+          'modules': {
+            'libp2p-tcp': {
+              'status': 'Done',
+              'url': 'https://github.com/libp2p/js-libp2p'
+            },
+            'libp2p-websockets': {
+              'status': 'Done',
+              'url': 'https://github.com/libp2p/js-libp2p'
+            },
+            'libp2p-webrtc-star': {
+              'status': 'Done',
+              'url': 'https://github.com/libp2p/js-libp2p'
+            }
+          }
+        },
+        "Stream Muxer": {
+          'modules': {
+            'libp2p-multiplex': {
+              'status': 'Done',
+              'url': ''
+            },
+            'libp2p-spdy': {
+              'status': 'Done',
+              'url': ''
+            }
+          }
+        },
+        "Crypto Channels": {
+          'modules': {
+            'libp2p-secio': {
+              'status': 'Done',
+              'url': ''
+            }
+          }
+        },
+        "Peer Routing": {
+          'modules': {
+            }
+        },
+        "Record Stores": {
+          'modules': {
+          }
+        },
+        "Discovery": {
+          'modules': {
+            'libp2p-mdns': {
+              'status': 'unstable',
+              'url': ''
+            },
+            'libp2p-webrtc-star': {
+              'status': 'unstable',
+              'url': ''
+            },
+            'libp2p-railing': {
+              'status': 'unstable',
+              'url': ''
+            }
+          }
+        }
+      }
+    },
+    'Node JS': {
+      'status': 'live',
+      'image': '../img/logo_2.png',
+      'github': 'https://github.com/libp2p/',
+      'categories': {
+        'Transport': {
+          'modules': {
+            'libp2p-websockets': {
+              'status': 'Done',
+              'url': 'https://github.com/libp2p/js-libp2p'
+            },
+            'libp2p-webrtc-star': {
+              'status': 'Done',
+              'url': 'https://github.com/libp2p/js-libp2p'
+            }
+          }
+        },
+        "Stream Muxer": {
+          'modules': {
+            'libp2p-multiplex': {
+              'status': 'Done',
+              'url': ''
+            },
+            'libp2p-spdy': {
+              'status': 'Done',
+              'url': ''
+            }
+          }
+        },
+        "Crypto Channels": {
+          'modules': {
+            'libp2p-secio': {
+              'status': 'Done',
+              'url': ''
+            }
+          }
+        },
+        "Peer Routing": {
+          'modules': {
+            }
+        },
+        "Record Stores": {
+          'modules': {
+            }
+        },
+        "Discovery": {
+          'modules': {
+            'libp2p-mdns': {
+              'status': 'unstable',
+              'url': ''
+            },
+            'libp2p-webrtc-star': {
+              'status': 'unstable',
+              'url': ''
+            },
+            'libp2p-railing': {
+              'status': 'unstable',
+              'url': ''
+            },
+          }
+        }
+      }
+    },
+    'Golang': {
+      'status': 'live',
+      'image': '../img/logo_3.png',
+      'github': 'https://github.com/libp2p/go-libp2p',
+      'categories': {
+        'Transport': {
+          'modules': {
+            'libp2p-tcp': {
+              'status': 'Done',
+              'url': 'https://github.com/libp2p/js-libp2p'
+            },
+            'libp2p-websockets': {
+              'status': 'Done',
+              'url': 'https://github.com/libp2p/js-libp2p'
+            }
+          }
+        },
+        "Stream Muxer": {
+          'modules': {
+            'libp2p-yamux': {
+              'status': 'Done',
+              'url': ''
+            },
+            'libp2p-multiplex': {
+              'status': 'Done',
+              'url': ''
+            }
+          }
+        },
+        "Crypto Channels": {
+          'modules': {
+            'libp2p-secio': {
+              'status': 'Done',
+              'url': ''
+            }
+          }
+        },
+        "Peer Routing": {
+          'modules': {
+            'libp2p-kad-dht': {
+              'status': 'Done',
+              'url': ''
+            }
+          }
+        },
+        "Record Stores": {
+          'modules': {
+            'libp2p-kad-dht': {
+              'status': 'Done',
+              'url': ''
+            }
+          }
+        },
+        "NAT Traversal": {
+          'modules': {
+          }
+        },
+        "Discovery": {
+          'modules': {
+            'libp2p-mdns': {
+              'status': 'unstable',
+              'url': ''
+            },
+            'libp2p-railing': {
+              'status': 'unstable',
+              'url': ''
+            }
+          }
+        }
+      },
+    'Haskell': {
+      'status': 'coming-soon',
+      'image': '../img/logo_4.png',
+      'github': ''
+    },
+    'Java': {
+      'status': 'coming-soon',
+      'image': '../img/logo_java.png',
+      'github': ''
+    },
+    'Python': {
+      'status': 'coming-soon',
+      'image': '../img/logo_6.png',
+      'github': ''
+    },
+    'Rust': {
+      'status': 'coming-soon',
+      'image': '../img/logo_7.png',
+      'github': ''
+    }
+}
